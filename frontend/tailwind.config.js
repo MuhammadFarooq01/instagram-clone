1fa5b7539e7b0889180fb0d48fb3f1dd8f7cfb40
Muhammad Farooq <farooq.dev13@gmail.com>
2024-07-24 20:04:45 +0500

    Added colors fontfamily and a custom breakpoint

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-medium": "#7F7F7F",
+        "blue-primary-hover": "#1877F2",
+        "blue-light": "#4BB4F8",
+        "blue-primary": "#0095f6",
+        "blue-dark": "#385185",
+        "gray-light": "#DBDBDB",
+      },
+      fontFamily: {
+        instagram: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      screens: {
+        cbp935: "935px",
+      },
+    },
   },
   plugins: [],
 };
